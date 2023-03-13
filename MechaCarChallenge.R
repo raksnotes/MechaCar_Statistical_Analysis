83b7037551e2d4df98348637726099f7a8cc2981
Raksha Karthikeyan <116187123+raksnotes@users.noreply.github.com>
2023-03-13 19:11:27 -0400

    Add files via upload

---
+++ MechaCarChallenge.R
@@ -0,0 +1,6 @@
+library(dplyr)
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_mpg)
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_mpg))
+
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_mpg))$r.squared
+
